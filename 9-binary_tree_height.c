9ef9e0423fac3721b298310f06d966c4e367d081
louay66 <louaylouay20@gmail.com>
2022-07-21 15:23:24 +0100

    test

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,17 +5,19 @@
  * @tree: tree of data
  *
  * Return: measures the height or 0
- * If tree is NULL
+ * 			If tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height_l;
+	size_t height_r;
+
 	if (!tree)
 		return (0);
-	size_t height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	size_t height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 
 	return (height_l > height_r ? height_l : height_r);
 
 }
-
