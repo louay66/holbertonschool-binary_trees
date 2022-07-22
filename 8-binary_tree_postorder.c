4497180fd1a30da2fdb5261cdcee71e090f6333d
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-07-22 03:03:03 -0700

    newcommit

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using
+ * binary_tree_postorder - goes through a binary tree using.
  * post-order traversal
  * @tree: tree to traverse
  * @func: pointer to a function to call for each node
