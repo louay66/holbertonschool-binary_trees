7ef1206dffe286b1b16b26f798f39a059843fdc4
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-07-22 03:01:55 -0700

    newcommit

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal.
  * @tree: tree to traverse
  * @func: pointer to a function to call for each node
  * Return: nothing
